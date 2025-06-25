264ba296ebb6aff5b230f2d68dde9c206370ddc4
Lemocuber <101167490+TMBMode@users.noreply.github.com>
2025-06-25 12:00:35 +0800

    Updated CustomCursor.tsx

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -45,7 +45,7 @@ const CustomCursor: React.FC = () => {
         top: mousePosition.y - 16,  // Center the 32px image (16px offset)
         width: '32px',
         height: '32px',
-        backgroundImage: `url(${RESOURCE_ENDPOINT}/RandomTimes/10.jpg)`,
+        backgroundImage: `url(${RESOURCE_ENDPOINT}/RandomTiles/10.jpg)`,
         backgroundSize: '32px 32px',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
