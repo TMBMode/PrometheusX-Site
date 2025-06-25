b7e4e95636d73b22c5fc81eae52d3becb0203685
Lemocuber <101167490+TMBMode@users.noreply.github.com>
2025-06-25 10:48:48 +0800

    Updated IntroPage.tsx

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -58,7 +58,7 @@ const IntroPage: React.FC<IntroPageProps> = ({ onEnter }) => {
           <img 
             src={`${RESOURCE_ENDPOINT}/Logo/logo-prometheusx.svg`}
             alt="PrometheusX Logo" 
-            className="h-auto w-24 hover:brightness-150 transition-all duration-300"
+            className="h-auto w-68 hover:brightness-150 transition-all duration-300"
           />
         </div>
         
