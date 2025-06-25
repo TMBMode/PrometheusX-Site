3df90c6f878086f1a8c24f04e87a6cb79ac3f8df
Lemocuber <101167490+TMBMode@users.noreply.github.com>
2025-06-25 18:08:20 +0800

    Updated IntroPage.tsx

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -57,7 +57,7 @@ const IntroPage: React.FC<IntroPageProps> = ({ onEnter }) => {
         <div className="absolute top-8 left-8">
           <Logo 
             className="hover:brightness-150 transition-all duration-300"
-            width={270}
+            width={280}
           />
         </div>
         
