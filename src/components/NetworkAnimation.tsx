6a42c168d62ca64edf30a0ba7674dd5eddbd1cc4
Lemocuber <101167490+TMBMode@users.noreply.github.com>
2025-06-25 19:16:20 +0800

    Updated NetworkAnimation.tsx

--- src/components/NetworkAnimation.tsx
+++ src/components/NetworkAnimation.tsx
@@ -404,7 +404,7 @@ const NetworkAnimation: React.FC<NetworkAnimationProps> = ({ containerRef }) => 
 
           {/* Rectangle */}
           <div
-            className={`absolute border-2 border-white transition-all duration-500 transform ${
+            className={`absolute border-2 border-white transition-all duration-500 transform hover:invert ${
               visibleRects.has(rect.id)
                 ? 'opacity-100 scale-100' 
                 : 'opacity-0 scale-75'
