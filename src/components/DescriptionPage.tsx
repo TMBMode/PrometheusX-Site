09776b441a461d31e40d5400717d0204a22c1359
Lemocuber <101167490+TMBMode@users.noreply.github.com>
2025-07-01 02:45:13 +0800

    Updated DescriptionPage.tsx

--- src/components/DescriptionPage.tsx
+++ src/components/DescriptionPage.tsx
@@ -24,13 +24,13 @@ const DescriptionPage: React.FC = () => {
             <p>
             Therefore, we initiate this movement not simply to react to the development of AI, but to
             </p>
-            <p className="pl-2">
+            <p className="pl-3">
             (1) Predict and characterize the trajectory of AI development through rigorous informatics modeling and interdisciplinary scientific inquiry
             </p>
-            <p className="pl-2">
+            <p className="pl-3">
             (2) Construct the feasibility and legitimacy of ontological argument of digital being
             </p>
-            <p className="pl-2">
+            <p className="pl-3">
             (3) Develop a scaffold that allows the rise of AI to be narrativized, practiced, resisted, and co-existed with in philosophical terms.
             </p>
           </div>
