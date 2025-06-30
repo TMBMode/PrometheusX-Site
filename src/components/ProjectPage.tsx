f9ecf0afa841447126339736393fc0c8c32aea91
Lemocuber <101167490+TMBMode@users.noreply.github.com>
2025-07-01 02:32:00 +0800

    Updated ProjectPage.tsx

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -4,7 +4,7 @@ const ProjectPage: React.FC = () => {
   return (
     <section className="relative min-h-screen w-full py-20 flex items-center">
       {/* Black Overlay */}
-      <div className="absolute inset-0 bg-black/20" />
+      <div className="absolute inset-0 bg-black/10" />
       
       <div className="relative z-10 max-w-2xl mx-auto px-8 landscape:w-[71.8%] landscape:mx-auto">
         <div className="text-center mb-12">
